23933328aad89293873f8838fffb2dfc3e254e8b
matheusrosini <matheussallisyahoo@gmail.com>
2025-11-11 21:19:23 -0300

    conectei o backend a um banco nuvem (railway)

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,23 @@
 const mysql = require("mysql2");
-require("dotenv").config(); // carrega variáveis do .env
+require("dotenv").config();
 
-// Cria conexão com o banco de dados
+// Cria conexão com o banco de dados Railway
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "projetoNotaDez",
-  port: process.env.DB_PORT || 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false } // aceita certificado autoassinado
 });
 
 // Tenta conectar
 connection.connect((err) => {
-  if (err) {
-    console.error("❌ Erro ao conectar ao MySQL:", err.message);
-  } else {
-    console.log("✅ Conectado ao MySQL");
-  }
+    if (err) {
+        console.error("❌ Erro ao conectar ao MySQL:", err.message);
+    } else {
+        console.log("✅ Conectado ao MySQL (Railway) com sucesso!");
+    }
 });
 
 module.exports = connection;
