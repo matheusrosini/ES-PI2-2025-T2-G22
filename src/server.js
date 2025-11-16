75a26f018bcf5023b261891011cf7246c28c3116
leddixd <leodionel06@gmail.com>
2025-11-15 22:08:49 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
-// Feito por Matheus Rosini
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-// Importação das rotas
-const usuarioRoutes = require("./routes/usuarioRoutes");
+const userRoutes = require("./routes/usuarioRoutes");
 const instituicaoRoutes = require("./routes/instituicaoRoutes");
-const disciplinaRoutes = require("./routes/disciplinasRoutes");
+const disciplinaRoutes = require("./routes/disciplinaRoutes");
 const turmaRoutes = require("./routes/turmaRoutes");
 const alunoRoutes = require("./routes/alunoRoutes");
 const notaRoutes = require("./routes/notaRoutes");
@@ -15,31 +12,22 @@ const componenteNotaRoutes = require("./routes/componenteNotaRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-
-app.use(
-    cors({
-        origin: "https://es-pi-2-2025-t2-g22.vercel.app",
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
-
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 
-// Rotas
-app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/usuarios", userRoutes);
 app.use("/api/instituicoes", instituicaoRoutes);
 app.use("/api/disciplinas", disciplinaRoutes);
 app.use("/api/turmas", turmaRoutes);
 app.use("/api/alunos", alunoRoutes);
 app.use("/api/notas", notaRoutes);
-app.use("/api/componenteNotas", componenteNotaRoutes);
+app.use("/api/componentes", componenteNotaRoutes);
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("API ProjetoNotaDez ONLINE!");
+    res.send("API está funcionando!");
 });
 
-module.exports = app;
+// ✔ AGORA SIM
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Rodando na porta " + PORT));
