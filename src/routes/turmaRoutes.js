e635ed739d7f8ad226d27cb331ed52d9145dd60d
leddixd <leodionel06@gmail.com>
2025-11-16 23:12:42 -0300

    Update turmaRoutes.js

--- src/routes/turmaRoutes.js
+++ src/routes/turmaRoutes.js
@@ -1,30 +1,11 @@
+// Feito por Leonardo e Matheus Rosini
+
 const express = require("express");
 const router = express.Router();
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 const turmaController = require("../controllers/turmaController");
-
-// Rotas da turma
-router.get("/", turmaController.getAllTurmas);
-router.get("/:id", turmaController.getTurmaById);
-router.post("/", turmaController.createTurma);
-router.put("/:id", turmaController.updateTurma);
-router.delete("/:id", turmaController.deleteTurma);
-
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
-const turmaController = require('../controllers/turmaController');
 const { requireAuth } = require('../middlewares/authmiddleware');
 
 router.use(requireAuth);
-=======
-const turmaController = require("../controllers/turmaController");
->>>>>>> e249d8c62d75e513def498a3b74a01006d08ff49
 
 // Rotas da turma
 router.get("/", turmaController.getAllTurmas);
@@ -33,11 +14,4 @@ router.post("/", turmaController.createTurma);
 router.put("/:id", turmaController.updateTurma);
 router.delete("/:id", turmaController.deleteTurma);
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 module.exports = router;
